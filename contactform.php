c31738bedcb9f647ff0396f537ed5ca1e7b35b1e
ClaesK37 <66672332+ClaesK37@users.noreply.github.com>
2020-09-27 15:26:33 +0200

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -1,24 +1,19 @@
 <?php
 
 if (isset($_POST["verzenden"])) {
-
+    $to = "claeskim33@hotmail.com";
     $name = $_POST['vnaam'];
     $achternaam = $_POST['achternaam'];
-    $visitor_email = $_POST['email'];
+    $from = $_POST['email'];
     $message = $_POST['vragen'];
 
-    $to = "claeskim33@hotmail.com";
+    
     $onderwerp = "ingevuld formulier";
-    $bericht = "Naam: .$name.\n"."Achternaam: ".$achternaam."\n". "Schreef: "."\n\n".$message;
+    $bericht = $name . " " . $achternaam . "schreef het volgende:" . "\n\n" . $message;
  
-    $headers="From: ".$email;
-    if(mail($to, $onderwerp, $bericht, $headers)) {
+    $headers="From: ". $from;
+    mail($to, $onderwerp, $bericht, $headers);
         echo "<h1>Verzonden!"." ".$name.", Ik contacteer je zo snel mogelijk.</h1>";
-    } else {
-        echo "oeps!";
-    }
-    header("locaction: index.html");
-
 }
 
 ?> 
