5d270b0431d0f572cd62fd4ce7f30a8c89f2e0d2
kim <claeskim33@hotmail.com>
2020-12-18 12:13:28 +0100

    test

---
+++ contactform.php
@@ -0,0 +1,19 @@
+<?php
+
+if (isset($_POST["verzenden"])) {
+    $to = "claeskim33@hotmail.com";
+    $name = $_POST['vnaam'];
+    $achternaam = $_POST['achternaam'];
+    $from = $_POST['email'];
+    $message = $_POST['vragen'];
+
+    
+    $onderwerp = "ingevuld formulier";
+    $bericht = $name . " " . $achternaam . "schreef het volgende:" . "\n\n" . $message;
+ 
+    $headers="From: ". $from;
+    mail($to, $onderwerp, $bericht, $headers);
+        echo "<h1>Verzonden!"." ".$name.", Ik contacteer je zo snel mogelijk.</h1>";
+}
+
+?> 
